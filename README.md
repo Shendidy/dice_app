abea4d75aa9d4345b92baf7b065c2597c6af0dda
Sherif Shendidy <shendidy@Sherifs-MacBook-Pro.local>
2019-01-10 11:28:49 +0000

    V1.1: Roll a dice

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 Sherif Shendidy 10/01/2019
 
+
 Working on the Dice TDD workshop
+
+
+V1.1:
+
+As a board game player,
+So that I can play a game
+I want to be able to roll a dice
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,4 +1,14 @@
 require 'dice'
 
 describe Dice do
+  context "V1.1: Roll a dice" do
+    it "Should  respond to .roll" do
+      dice = Dice.new
+      expect(dice).to respond_to :roll
+    end
+    it "Should return a random number between 1 & 6" do
+      dice = Dice.new
+      expect(dice.roll).to be < 7
+    end
+  end
 end
